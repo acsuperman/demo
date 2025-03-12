a79ee9beb5cb26f9e56ae21c7e08272de03f7a56
acsuperman <123.free>
2025-03-12 16:41:19 +0800

    11

---
+++ demo/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+name: "AboutView.vue",
+  data(){
+return{
+  query:""
+} },
+    created(){
+this.query=this.$route.query.query;
+    }
+
+}
+</script>
+
+<template>
+  <div>
+    {{query}}
+  </div>
+</template>
+
+<style scoped>
+
+</style>

618e50f6351cb9237df7487fcadc7a2cc1201729
acsuperman <123.free>
2025-03-12 16:40:46 +0800

    11

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,12 +10,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/layout',
+    component:()=>import( '../views/LayoutView.vue'),
+    children: [
+      {
+        path: 'about',
+        component: () => import( '../views/AboutView.vue')
+      }
+    ]
   }
 ]
 
---
+++ demo/src/views/LayoutView.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "LayoutView.vue",
+  methods: {
+    onClick(now) {
+      this.$router.push({
+        path: "/layout/about",
+        query: {
+          query: now
+        }
+      }).catch((error) => {
+        // 检查是否为 NavigationDuplicated 错误
+        if (error.name === 'NavigationDuplicated') {
+          // 忽略该错误
+          return;
+        }
+        // 其他错误继续抛出
+        throw error;
+      });
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <div style="height: 50%;width: 100%">
+      <router-view></router-view>
+    </div>
+
+    <button @click="onClick('123')">123</button>
+    <button @click="onClick('456')">456</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
